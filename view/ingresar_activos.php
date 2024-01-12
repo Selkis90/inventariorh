550afc183fd4aa5d9a6db7454d73101cafb69ce4
selkis90 <serlkis90@gmail.com>
2024-01-11 20:15:38 -0500

    se crea el archivo ingresar_activos.php

---
+++ view/ingresar_activos.php
@@ -0,0 +1,9 @@
+<?php
+    include_once '../conexion.php';
+?>
+<ul>
+    <li><a href="crear_ingresar_activos.php"></a></li>
+    <li><a href="view_ingresar_activos.php"></a></li>
+    <li><a href="update_ingresar_activos.php"></a></li>
+    <li><a href="delete_ingresar_activos.php"></a></li>
+</ul>
