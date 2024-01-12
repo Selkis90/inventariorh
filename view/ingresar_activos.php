0fff71f028462909ee51641f2563a448b53cb960
selkis90 <serlkis90@gmail.com>
2024-01-11 20:48:44 -0500

    correcion de require_once '../header.php'

--- view/ingresar_activos.php
+++ view/ingresar_activos.php
@@ -1,9 +1,13 @@
 <?php
-    include_once '../conexion.php';
+    include_once '../header.php';
 ?>
 <ul>
-    <li><a href="crear_ingresar_activos.php"></a></li>
-    <li><a href="view_ingresar_activos.php"></a></li>
-    <li><a href="update_ingresar_activos.php"></a></li>
-    <li><a href="delete_ingresar_activos.php"></a></li>
+    <li><a href="create_ingresar_activos.php">Ingresar Activo</a></li>
+    <li><a href="view_ingresar_activos.php">Ver Activos </a></li>
+    <li><a href="update_ingresar_activos.php">Actualizar Activos</a></li>
+    <li><a href="delete_ingresar_activos.php">Eliminar Activos</a></li>
 </ul>
+
+<?php
+    require_once '../footer.php';
+?>
