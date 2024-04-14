b938665ec6f7076aa543546a918b3a9e0cea1b14
Selkis90 <selkis@gmail.com>
2024-04-14 12:49:39 -0500

    feat(responsive)se ajustan responsive de inicio a disintos dispositivo

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ require_once "header_login.php";
 <!-- <a href="/view/view_registrarse.php">Registrarse</a> <br>
 <a href="/view/view_inicio.php">Iniciar Seccion</a> -->
 
-<a id="inicio" href="/view/view_inicio.php">INVENTARIORH</a>
+<div class="contenedor">
+    <a id="inicio" href="/view/view_inicio.php">INVENTARIORH</a>
+</div>
 
 <?php
 /* include("footer.php"); */
