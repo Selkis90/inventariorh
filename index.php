f93a99263680da97b4cc01185dea5af24d85a7e4
Selkis90 <selkis@gmail.com>
2024-07-21 20:02:31 -0500

    feat index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once "header_login.php";
 <div class="BTN_INICIO">
 <a id="inicio" href="/view/view_inicio.php"><img src="/img/logo inventariorh.jpg" alt="logo INVENTARIORH"></a>
 </div>
-<h1>hola</h1>
+
 
 <?php
 /* include("footer.php"); */
