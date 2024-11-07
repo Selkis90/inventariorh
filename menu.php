f1649482a63db6850141da6d4e7560ccf22a15c6
As <angelsv@hotmail.com>
2024-11-06 21:04:41 -0500

    añade funcionalidad para mostrar y ocultar secciones en menu.php mediante atributos de datos

--- menu.php
+++ menu.php
@@ -12,22 +12,25 @@
                 <a class="nav-link" href="<?= _URL ?>principal.php">Inicio</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="Proveedor">Proveedor</a>
+                <a class="nav-link link-show-section" data-show-section="proveedor_oculto" href="#" id="Proveedor">Proveedor</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="Producto">Producto</a>
+                <a class="nav-link link-show-section" data-show-section="producto_oculto" href="#" id="Producto">Producto</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="stock">Stock</a>
+                <a class="nav-link link-show-section" data-show-section="stock_oculto" href="#" id="stock">Stock</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="Activos">Activos</a>
+                <a class="nav-link link-show-section" data-show-section="activo_oculto" href="#" id="Activos">Activos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="Translados">Translados</a>
+                <a class="nav-link link-show-section" data-show-section="translado_oculto" href="#" id="Translados">Translados</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" id="ajustes_inventario">Ajustes Inventario</a>
+                <a class="nav-link link-show-section" data-show-section="Ajustes_Inventario_oculto" href="#" id="ajustes_inventario">Ajustes Inventario</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link-show-section" data-show-section="nuevo_section" href="#" id="ajustes_inventario">Nuevo section</a>
             </li>
         </ul>
 
