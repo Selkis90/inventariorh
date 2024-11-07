c8544dc7d9c5a61db51b8048c77694fc22300132
As <angelsv@hotmail.com>
2024-11-06 21:10:20 -0500

    actualiza enlaces en principal.php para utilizar la constante _URL

--- principal.php
+++ principal.php
@@ -14,28 +14,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_proveedor.php">Crear Proveedor</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_proveedor.php">Crear Proveedor</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_proveedor.php">Ver Proveedor</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_proveedor.php">Ver Proveedor</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_proveedor.php">Actualizar Proveedor</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_proveedor.php">Actualizar Proveedor</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_proveedor.php">Eliminar Proveedor</a></li>
+                <li><a href="<?= _URL ?>view/delete_proveedor.php">Eliminar Proveedor</a></li>
             </ul>
         </div>
     </article>
@@ -49,28 +49,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_producto.php">Crear Producto</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_producto.php">Crear Producto</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_producto.php">Ver Producto</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_producto.php">Ver Producto</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_producto.php">Actualizar Producto</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_producto.php">Actualizar Producto</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_producto.php">Eliminar Producto</a></li>
+                <li><a href="<?= _URL ?>view/delete_producto.php">Eliminar Producto</a></li>
             </ul>
         </div>
     </article>
@@ -83,28 +83,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_stock.php">Crear Stock</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_stock.php">Crear Stock</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_stock.php">Ver Stock</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_stock.php">Ver Stock</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_stock.php">Actualizar Stock</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_stock.php">Actualizar Stock</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_stock.php">Eliminar Stock</a></li>
+                <li><a href="<?= _URL ?>view/delete_stock.php">Eliminar Stock</a></li>
             </ul>
         </div>
     </article>
@@ -119,28 +119,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_activos.php">Crear Activos</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_activos.php">Crear Activos</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_activos.php">Ver Activos</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_activos.php">Ver Activos</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_activos.php">Actualizar Activos</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_activos.php">Actualizar Activos</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_activos.php">Eliminar Activos</a></li>
+                <li><a href="<?= _URL ?>view/delete_activos.php">Eliminar Activos</a></li>
             </ul>
         </div>
     </article>
@@ -154,28 +154,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_translados.php">Crear Translados</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_translados.php">Crear Translados</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_translados.php">Ver Translados</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_translados.php">Ver Translados</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_translados.php">Actualizar Translados</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_translados.php">Actualizar Translados</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_translados.php">Eliminar Translados</a></li>
+                <li><a href="<?= _URL ?>view/delete_translados.php">Eliminar Translados</a></li>
             </ul>
         </div>
     </article>
@@ -191,28 +191,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_Ajustes_inventario.php">Crear Ajustes</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_Ajustes_inventario.php">Crear Ajustes</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_Ajustes_Inventario.php">Ver Ajustes</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_Ajustes_Inventario.php">Ver Ajustes</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_Ajustes_Inventario.php">Actualizar Ajustes</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_Ajustes_Inventario.php">Actualizar Ajustes</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_Ajustes_Inventario.php">Eliminar Ajustes</a></li>
+                <li><a href="<?= _URL ?>view/delete_Ajustes_Inventario.php">Eliminar Ajustes</a></li>
             </ul>
         </div>
     </article>
@@ -227,28 +227,28 @@ require_once 'menu.php';
         <div>
             <ul>
                 <!-- Enlace para crear nuevo stock -->
-                <li><a href="./view/create_compras.php">Crear Compras</a></li><!-- Enlace para ver el stock existente -->
+                <li><a href="<?= _URL ?>view/create_compras.php">Crear Compras</a></li><!-- Enlace para ver el stock existente -->
             </ul>
         </div>
     </article>
     <article class="article2">
         <div>
             <ul>
-                <li><a href="./view/view_compras.php">Ver Compras</a></li><!-- Enlace para actualizar información de stock -->
+                <li><a href="<?= _URL ?>view/view_compras.php">Ver Compras</a></li><!-- Enlace para actualizar información de stock -->
             </ul>
         </div>
     </article>
     <article class="article3">
         <div>
             <ul>
-                <li><a href="./view/update_compras.php">Actualizar Compras</a></li><!-- Enlace para eliminar entradas de stock -->
+                <li><a href="<?= _URL ?>view/update_compras.php">Actualizar Compras</a></li><!-- Enlace para eliminar entradas de stock -->
             </ul>
         </div>
     </article>
     <article class="article4">
         <div>
             <ul>
-                <li><a href="./view/delete_compras.php">Eliminar Compras</a></li>
+                <li><a href="<?= _URL ?>view/delete_compras.php">Eliminar Compras</a></li>
             </ul>
         </div>
     </article>
