342d2ad66db37ad10160227772d9a5dadd1ff8f0
As <angelsv@hotmail.com>
2024-11-06 21:04:36 -0500

    mejora la gestión de secciones en principal.js añadiendo funcionalidad para ocultar y mostrar secciones con botones de cierre

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,32 +1,58 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Obtenemos todos los enlaces que queremos manejar
-  var enlaces = [
-    { id: "Proveedor", objetivo: "proveedor_oculto" },
-    { id: "Producto", objetivo: "producto_oculto" },
-    { id: "stock", objetivo: "stock_oculto" },
-    { id: "Activos", objetivo: "activo_oculto" },
-    { id: "Translados", objetivo: "translado_oculto" },
-    { id: "ajustes_inventario", objetivo: "Ajustes_Inventario_oculto" },
-    { id: "Compras", objetivo: "compra_oculto" },
-  ];
+
+  // añadir evento click a los enlaces .link-show-section
+  var enlaces = document.querySelectorAll(".link-show-section");
 
   // Iteramos sobre cada enlace y añadimos el evento 'click'
   enlaces.forEach(function (enlace) {
-    var enlaceElemento = document.getElementById(enlace.id);
 
-    enlaceElemento.addEventListener("click", function (event) {
+    enlace.addEventListener("click", function (event) {
       event.preventDefault();
 
       // Ocultamos todas las secciones primero
-      enlaces.forEach(function (otroEnlace) {
-        var otraSeccion = document.getElementById(otroEnlace.objetivo);
-        otraSeccion.style.display = "none";
-      });
+      ocultarSections();
 
-      // Mostramos solo la sección correspondiente al enlace actual
-      var seccionMostrar = document.getElementById(enlace.objetivo);
+      // obtener el valor del atributo 'data-show-section'
+      var dataShowSection = enlace.getAttribute("data-show-section");
+
+      // Mostrar el section con el id #'dataShowSection'
+      var seccionMostrar = document.getElementById(dataShowSection);
       seccionMostrar.style.display = "block";
+
+      // Añadir botón cerrar a la sección
+      addCloseButtonToSection(dataShowSection);
     });
   });
 });
+
+// Función para ocultar todas las secciones
+function ocultarSections() {
+  removeItemsByClass(".remove-button");
+
+  var sections = document.querySelectorAll(".section-hide-me");
+  sections.forEach(function (otroEnlace) {
+    otroEnlace.style.display = "none";
+  });
+}
+
+
+function addCloseButtonToSection(idSection) {
+  var section = document.getElementById(idSection);
+  var closeButton = document.createElement("button");
+  // añadir clase remove-button
+  closeButton.classList.add("remove-button");
+  closeButton.textContent = "Cerrar";
+  closeButton.addEventListener("click", function () {
+    section.style.display = "none";
+  });
+  // pegar al inicio del section
+  section.insertBefore(closeButton, section.firstChild);
+}
+
+function removeItemsByClass(className) {
+  var items = document.querySelectorAll(className);
+  items.forEach(function (item) {;
+    item.remove();
+  });
+}
